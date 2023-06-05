b1ab1ba4ad9139f1c3ef7d9905a11af8f9a9cec9
Gabriel-Carvf <39717664+GabrielCarvf@users.noreply.github.com>
2023-06-05 07:30:49 -0300

    :hammer: Configuração inicial do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
+import { GlobalStyles } from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
+  GlobalStyles();
+
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  border: 0,
+  padding: '1rem',
+  borderRadius: 5,
+})
+
 export default function Home() {
   return (
-    <h1> Teste do Next.js</h1>
+    <Button> <h1>Teste do Next.js</h1></Button>
   )
 }
